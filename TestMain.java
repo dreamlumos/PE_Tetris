9fd29cd254e7a1f0b0622a996869808b9f7ed03f
dreamlumos <30956230+dreamlumos@users.noreply.github.com>
2020-01-06 14:51:10 +0100

    Main fix

--- TestMain.java
+++ TestMain.java
@@ -8,26 +8,22 @@ public class TestMain{
 
 	public static void main(String[] args){
 
-	Gameboard gameboard = new Gameboard(20,8,30);
-	LeftSidebar lsb = new LeftSidebar(gameboard);
-	RightSidebar rsb = new RightSidebar(gameboard);
-
         HoldQueue holdQueue = new HoldQueue(TILE_SIZE);
         Gameboard gameboard = new Gameboard(20, 8, TILE_SIZE, holdQueue);
         LeftSidebar lsb = new LeftSidebar(gameboard, holdQueue);
         RightSidebar rsb = new RightSidebar(gameboard);
 
-	Window window = new Window(gameboard, lsb, rsb);
-	while(!gameboard.getEndofgame()){
-  		window.repaint();
-		try{
-			Thread.sleep(200);
-		} catch (Exception e){
-			System.out.println(e);
-		}
+    	Window window = new Window(gameboard, lsb, rsb);
+    	while(!gameboard.getEndofgame()){
+      		window.repaint();
+    		try{
+    			Thread.sleep(200);
+    		} catch (Exception e){
+    			System.out.println(e);
+    		}
 
-		gameboard.getTetromino().softDrop();
-            
-        }
+    		gameboard.getTetromino().softDrop();
+                
+            }
 	}
 }
